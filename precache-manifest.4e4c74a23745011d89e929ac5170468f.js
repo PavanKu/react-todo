40d9225f5c23604f21687ddf36d5be0d03356cec
PavanKu <PavanKu@users.noreply.github.com>
2020-06-11 16:01:48 +0000

    deploy: 867e6ba1a141a44f8b24fc4681bb38810e11bc2a

---
+++ precache-manifest.4e4c74a23745011d89e929ac5170468f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0802f9e43c3a2c28c9ecc0711b03af6",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "6dcdb84d4ae989b68175",
+    "url": "/react-todo/static/js/2.19cc5c74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.19cc5c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f7b3e2ac9c6f90b200",
+    "url": "/react-todo/static/js/main.48d432f4.chunk.js"
+  },
+  {
+    "revision": "f6b38ae6aff569678342",
+    "url": "/react-todo/static/js/runtime-main.3a81e4f2.js"
+  }
+]);
